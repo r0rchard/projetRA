ccff32d89b2926e2b1711a6f885b5189e6e0e7c9
nicolas delcombel <36881822+r0rchard@users.noreply.github.com>
2018-11-25 18:21:51 +0100

    vidéo avec la main visibke

--- main.m
+++ main.m
@@ -1,15 +1,17 @@
-for i=1:10:n
+%%
+aviobj=VideoWriter('video.avi','Uncompressed AVI');
+aviobj.FrameRate=5;
+open(aviobj)
+ for i=1:10:n
     %On récupère chaque frame de la vidéo
+
     video = read(obj,i);
     %On récupère les dimensions de cette frame (hauteur et largeur)
     %[hautVid,largVid]=size(video);
     hautVid=size(video,1);
     largVid=size(video,2);
     
-    %division chaque matrice
-    videoR=video(:,:,1);
-    videoG=video(:,:,2);
-	videoB=video(:,:,3);
+
     
     %On stocke les coordonnées des coins de la feuille
     coinVidHG=[historiqueAngles(i,1),historiqueAngles(i,2)];
@@ -17,15 +19,15 @@ for i=1:10:n
     coinVidBG=[historiqueAngles(i,5),historiqueAngles(i,6)];
     coinVidBD=[historiqueAngles(i,7),historiqueAngles(i,8)];
     
-    %On cherche l'homographie
-    %Passage de l'image en noir et blanc pour ne travailler que sur une matrice
-    image = imread('Clifford.jpg');
-    imageR=image(:,:,1);
-    imageG=image(:,:,2);
-	imageB=image(:,:,3);
+
     
+   
+
+%-------
+    
+    image = imread('Clifford.jpg');
+
     %On récupère les dimensions de l'image
-    %[hautImg,largImg]=size(image);
     hautImg=size(image,1);
     largImg=size(image,2);
     %Stockage des coordonnées des 4 coins de l'image
@@ -34,46 +36,128 @@ for i=1:10:n
     coinImgBG = [1,hautImg];
     coinImgBD = [largImg,hautImg];
     
+    
     %Résolution de l'équation pour retrouver Hp
     
     %Calcul de l'homographie entre deux plans à partir de 8 points
     matH = homography(coinVidHG,coinVidHD,coinVidBG,coinVidBD,coinImgHG,coinImgHD,coinImgBG,coinImgBD);
     
-    %Création d'une grille de dimension égale à celles de la frame de la vidéo
-    [A,B]=meshgrid(1:largVid,1:hautVid);
+    %coordonées des points projetés sur l'image
+    [X1,Y1,X2,Y2] = estimation(largVid,hautVid,matH);
     
-    %creation matrices
-    dim=largVid*hautVid;
-    X1 = reshape(A,[1,dim]);
-    Y1 = reshape(B,[1,dim]);
-    O = ones(1,dim);
-    X1p = vertcat(X1,Y1,O);
-    
-    X2p=matH*X1p; %homographie
-    
-    X2=uint16(X2p(1,:)./X2p(3,:));
-    Y2=uint16(X2p(2,:)./X2p(3,:));
-    
-    %find /!\ correspondance
-    pos = intersect(intersect(find(Y2>=1),find(Y2<=hautImg)),intersect(find(X2>=1),find(X2<=largImg)));
-    
-    X4=X2(pos);
-    Y4=Y2(pos);
-    
-    X3=X1(pos);
-    Y3=Y1(pos);
+    %coordonées finales
+    %X3,Y3 : coordonées des points de la vidéo
+    %X4,Y4 : coordonées des points de l'image
+    [X3,Y3,X4,Y4]=position(X1,Y1,X2,Y2,hautImg,largImg);
 
-%     posVideo = Y3 + (X3 - 1)*hautVid;
-%     posImg = Y4 + (X4 - 1)*hautImg;
-%     video([(posVideo) (posVideo + largVid*hautVid) (posVideo + 2*largVid*hautVid)]) = image([(posImg) (posImg + largImg*hautImg) (posImg + 2*largImg*hautImg)]);
-%     video(posVideo)=image(posImg);
-    for x=1:length(X3)
-%       video(Y3(x),X3(x))=image(Y4(x),X4(x));
-        videoR(Y3(x),X3(x))=imageR(Y4(x),X4(x));
-        videoB(Y3(x),X3(x))=imageB(Y4(x),X4(x));
-        videoG(Y3(x),X3(x))=imageG(Y4(x),X4(x));
+    %image finale
+    video2 = projection(image,video,X3,Y3,X4,Y4);
+%%
+
+
+
+    % détection main
+    
+    %zone de la main
+    
+    coinZHG = [coinVidHG(1)+round(3/4*(coinVidHD(1)-coinVidHG(1))),coinVidHD(2)+round(1/4*(coinVidHG(2)-coinVidHD(2)))];
+    coinZHD = coinVidHD;
+    coinZBG = [coinVidBG(1)+round(3/4*(coinVidBD(1)-coinVidBG(1))),coinVidBD(2)+round(1/4*(coinVidBG(2)-coinVidBD(2)))];
+    coinZBD = coinVidBD;
+    
+    % création M
+    largM=360;
+    hautM=1080;
+    M = ones(hautM,largM);
+    
+    %Stockage des coordonnées des 4 coins de M
+    coinMHG = [1,1];
+    coinMHD = [largM,1];
+    coinMBG = [1,hautM];
+    coinMBD = [largM,hautM];
+    
+    %Taille de la zone
+    
+    %Homographie de la zone vers M
+    matH2 = homography(coinZHG,coinZHD,coinZBG,coinZBD,coinMHG,coinMHD,coinMBG,coinMBD);
+    
+    %Projecion des pixels de la vidéo sur M
+    [X5,Y5,X6,Y6] = estimation(largVid,hautVid,matH2);
+    
+    %Correspondances entre les pixels de la zone et de M
+    [X7,Y7,X8,Y8]=position(X5,Y5,X6,Y6,hautM,largM);
+    
+    % test X7Y7
+%     mask2 = zeros(hautVid,largVid);
+%     for x=1:length(X7)
+%         mask2(Y7(x),X7(x))=1;
+%     end
+%     figure, imshow(mask2);
+
+    %test X8Y8
+%     mask3 = zeros(hautM,largM);
+%     for x=1:length(X8)
+%         mask3(Y8(x),X8(x))=1;
+%     end
+%     figure('Name','mask 3'), imshow(mask3);
+    
+    %Projection des pixels de la zone dans M2
+    videoR=video(:,:,1);
+    videoG=video(:,:,2);
+	videoB=video(:,:,3);
+    
+    N = uint8(ones(hautM,largM,3));
+%     figure('Name','N'), imshow(N(:,:,1));
+    M2 = projection(video,N,X8,Y8,X7,Y7);
+%     figure('Name','M2'), imshow(M2);
+%       
+      
+    
+%     %filtrage
+    y=0.299*M2(:,:,1)+0.587*M2(:,:,2)+0.114*M2(:,:,3); 
+    Cb1=0.564*(M2(:,:,3)-y)+128; 
+    M3=double(Cb1>128);
+    %figure('Name','M3'), imshow(M3); % vérification filtre
+
+%     Cb1 = double(M2(:,:,2)<150);
+%     Cb2 = double(M2(:,:,3)<150);
+%     figure('Name','Cb1'), imshow(Cb1);
+%     figure('Name','Cb2'), imshow(Cb2);
+%%
+    % création du masque
+%     mask = zeros(hautVid,largVid);
+%     for x=1:length(X3)
+%         mask(Y3(x),X3(x))=1;
+%     end
+%     figure('Name','mask'), imshow(mask);%vérification masque
+    
+    %Projection du masque sur la vidéo
+    %vérification masque M projeté
+%     mask4 = zeros(hautVid,largVid);
+%     for x=1:length(X7)
+%         if M3(Y8(x),X8(x))==1
+%            mask4(Y7(x),X7(x))=1;
+%         end
+%     end
+%     figure('Name','mask4'), imshow(mask4);
+            
+    video3R=video2(:,:,1);
+    video3G=video2(:,:,2);
+	video3B=video2(:,:,3);
+    vR=video(:,:,1);
+    vG=video(:,:,2);
+	vB=video(:,:,3);
+    for x=1:length(X7)
+        if M3(Y8(x),X8(x))==0
+        video3R(Y7(x),X7(x))=vR(Y7(x),X7(x));
+        video3B(Y7(x),X7(x))=vG(Y7(x),X7(x));
+        video3G(Y7(x),X7(x))=vB(Y7(x),X7(x));
+        end
     end
-    video2=cat(3,videoR,videoG,videoB);
-    imshow(video2);
-    refresh;
-end
+    
+    
+    video3=cat(3,video3R,video3G,video3B);
+    writeVideo(aviobj, video3);
+%     figure('Name','video3'),imshow(video3);
+ end
+ close(aviobj);
